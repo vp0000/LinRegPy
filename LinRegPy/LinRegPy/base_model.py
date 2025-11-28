6bea3a53e332c390ec86df8db4f9a69913c1c55f
vp0000 <varun3122000@gmail.com>
2025-11-28 13:09:05 +0530

    Refactor LinearRegSuper class with improved structure

--- LinRegPy/LinRegPy/base_model.py
+++ LinRegPy/LinRegPy/base_model.py
@@ -265,8 +265,8 @@ class LinearRegSuper:
         for key, value in eval_metrics.items():
             diag_lines.append(f"{key}: {float_fmt.format(value)}")
         for key, value in model_diag.items():
+            diag_lines.append(f"{key}":)
             for k, v in value.items():
-                diag_lines.append("\n")
                 diag_lines.append(f"{k.split('_')[1]}: {float_fmt.format(v)}")
 
         # Coefficient DataFrame
@@ -385,6 +385,7 @@ class LinearRegSuper:
         # default generic names
 
         return [f"x{i+1}" for i in range(X.shape[1])]
+
 
 
 
