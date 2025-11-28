6ea78340cce5a3d9af69f8b53751dea8f2059cea
vp0000 <varun3122000@gmail.com>
2025-11-28 13:07:13 +0530

    Minor fixes to LinearRegSuper class

--- LinRegPy/LinRegPy/base_model.py
+++ LinRegPy/LinRegPy/base_model.py
@@ -265,7 +265,7 @@ class LinearRegSuper:
         for key, value in eval_metrics.items():
             diag_lines.append(f"{key}: {float_fmt.format(value)}")
         for key, value in model_diag.items():
-            for k, v in val.items():
+            for k, v in value.items():
                 diag_lines.append("\n")
                 diag_lines.append(f"{k.split('_')[1]}: {float.fmt.format(v)}")
 
@@ -385,6 +385,7 @@ class LinearRegSuper:
         # default generic names
 
         return [f"x{i+1}" for i in range(X.shape[1])]
+
 
 
 
